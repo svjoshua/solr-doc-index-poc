a3f8094814630e810e1e47f1d44e65f1cef92bb1
Joshua <jmoor@simpleviewinc.com>
2019-06-14 12:14:31 -0700

    Initial Commit

---
+++ scripts/prepServer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ `whoami` != "root" ]; then
+	echo "Script must be executed as root"
+	exit 1
+fi
+TITLE='[35m'
+INPUT='[32m'
+RESET='[0m'
+
+echo -e "\e${TITLE} -=Configure Security=- \e${RESET}"
+echo -e "\e${TITLE} --Updating \e${RESET}"
+cp /sv/limits.conf /etc/security/limits.confcd
+echo -e "\e${TITLE} -=Security Config Complete=- \e${RESET}"
---
+++ scripts/setupServer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [ `whoami` != "root" ]; then
+	echo "Script must be executed as root"
+	exit 1
+fi
+TITLE='[35m'
+INPUT='[32m'
+NOTICE='[33m'
+RESET='[0m'
+
+echo -e "\e${TITLE} -=Setup SOLR Demo=- \e${RESET}"
+echo -e "\e${TITLE} --Prep \e${RESET}"
+#apt-get update
+
+echo -e "\e${TITLE} --Downloading SOLR \e${RESET}"
+#mkdir /solr
+#wget -nd http://apache.mirrors.tds.net/lucene/solr/8.1.1/solr-8.1.1.tgz -P /solr
+echo -e "\e${TITLE} --Unpacking SOLR \e${RESET}"
+#tar -zxvf /solr/solr-8.1.1.tgz
+
+echo -e "\e${TITLE} --Installing Java \e${RESET}"
+#apt-get install default-jdk -y
+
+echo -e "\e${TITLE} --Starting SOLR \e${RESET}"
+/solr/solr-8.1.1/bin/solr start -e cloud -noprompt -force
+echo -e "\e${TITLE} --Building Collection \e${RESET}"
+/solr/solr-8.1.1/bin/solr create -c docIndex -s 2 -rf 2
+echo -e "\e${TITLE} --Indexing Documents \e${RESET}"
+/solr/solr-8.1.1/bin/post -c docIndex /sv/docs
+
+echo -e "\e${TITLE} -=End Setup=- \e${RESET}"
+
+echo -e "\e${NOTICE} Access SOLR UI via the browser at http://192.168.50.203:8983/solr/#/docIndex/query \e${RESET}"
